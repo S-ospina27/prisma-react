2342a98cacd3700aa7a27bff8315f9f7fce919a2
Sergio León <sergioleon4004@hotmail.com>
2022-12-28 10:20:03 -0500

    refactor: variables have been renamed

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -19,6 +19,7 @@ function Users() {
   const [openRegister, setOpenRegister] = useState(false);
   const [users, setUsers] = useState([]);
 
+  const [idusers, setIdusers] = useState("");
   const [idroles, setIdroles] = useState("");
   const [iddocument_types, setIddocument_types] = useState("");
   const [users_identification, setUsers_identification] = useState("");
@@ -34,46 +35,56 @@ function Users() {
   const [users_contact_phone, setUsers_contact_phone] = useState("");
   const [idstatus, setIdstatus] = useState(1);
 
-  const [idusers_a, setIdusers_a] = useState("");
-  const [idroles_a, setIdroles_a] = useState("");
-  const [iddocument_types_a, setIddocument_types_a] = useState("");
-  const [users_identification_a, setUsers_identification_a] = useState("");
-  const [users_name_a, setUsers_name_a] = useState("");
-  const [users_lastname_a, setUsers_lastname_a] = useState("");
-  const [users_phone_a, setUsers_phone_a] = useState("");
-  const [users_address_a, setUsers_address_a] = useState("");
-  const [iddepartments_a, setIddepartments_a] = useState("");
-  const [idcities_a, setIdcities_a] = useState("");
-  const [users_email_a, setUsers_email_a] = useState("");
-  const [users_contact_name_a, setUsers_contact_name_a] = useState("");
-  const [users_contact_phone_a, setUsers_contact_phone_a] = useState("");
-  const [idstatus_a, setIdstatus_a] = useState("");
-
   const handleReadUsers = () => {
     axios.get(RoutesList.api.users.read.index).then((res) => {
       setUsers(res.data);
     });
   };
 
-  const setFields = (row) => {
-    setIdusers_a(row.idusers);
-    setIdroles_a(row.idroles);
-    setIddocument_types_a(row.iddocument_types);
-    setUsers_identification_a(row.users_identification);
-    setUsers_name_a(row.users_name);
-    setUsers_lastname_a(row.users_lastname);
-    setUsers_phone_a(row.users_phone);
-    setUsers_address_a(row.users_address);
-    setIddepartments_a(`${row.iddepartments} - ${row.departments_name}`);
-    setIdcities_a(`${row.idcities} - ${row.cities_name}`);
-    setUsers_email_a(row.users_email);
-    setUsers_contact_name_a(
+  const setFields = (
+    row = {
+      idusers: "",
+      idroles: "",
+      iddocument_types: "",
+      users_identification: "",
+      users_name: "",
+      users_lastname: "",
+      users_phone: "",
+      users_address: "",
+      iddepartments: "",
+      departments_name: "",
+      idcities: "",
+      cities_name: "",
+      users_email: "",
+      users_contact_name: "",
+      users_contact_phone: "",
+      idstatus: 1,
+    }
+  ) => {
+    setIdusers(row.idusers);
+    setIdroles(row.idroles);
+    setIddocument_types(row.iddocument_types);
+    setUsers_identification(row.users_identification);
+    setUsers_name(row.users_name);
+    setUsers_lastname(row.users_lastname);
+    setUsers_phone(row.users_phone);
+    setUsers_address(row.users_address);
+    setIddepartments(
+      row.iddepartments === ""
+        ? ""
+        : `${row.iddepartments} - ${row.departments_name}`
+    );
+    setIdcities(
+      row.idcities === "" ? "" : `${row.idcities} - ${row.cities_name}`
+    );
+    setUsers_email(row.users_email);
+    setUsers_contact_name(
       row.users_contact_name === null ? "" : row.users_contact_name
     );
-    setUsers_contact_phone_a(
+    setUsers_contact_phone(
       row.users_contact_phone === null ? "" : row.users_contact_phone
     );
-    setIdstatus_a(row.idstatus);
+    setIdstatus(row.idstatus);
   };
 
   const handleCreateUsers = (e) => {
@@ -108,19 +119,19 @@ function Users() {
     e.preventDefault();
 
     const form = new FormData();
-    form.append("idusers", idusers_a);
-    form.append("idroles", idroles_a);
-    form.append("iddocument_types", iddocument_types_a);
-    form.append("users_identification", users_identification_a);
-    form.append("users_name", users_name_a);
-    form.append("users_lastname", users_lastname_a);
-    form.append("users_phone", users_phone_a);
-    form.append("users_address", users_address_a);
+    form.append("idusers", idusers);
+    form.append("idroles", idroles);
+    form.append("iddocument_types", iddocument_types);
+    form.append("users_identification", users_identification);
+    form.append("users_name", users_name);
+    form.append("users_lastname", users_lastname);
+    form.append("users_phone", users_phone);
+    form.append("users_address", users_address);
     form.append("idcities", idcities_a.split("-").shift().trim());
-    form.append("users_email", users_email_a);
-    form.append("users_contact_name", users_contact_name_a);
-    form.append("users_contact_phone", users_contact_phone_a);
-    form.append("idstatus", idstatus_a);
+    form.append("users_email", users_email);
+    form.append("users_contact_name", users_contact_name);
+    form.append("users_contact_phone", users_contact_phone);
+    form.append("idstatus", idstatus);
 
     axios.post(RoutesList.api.users.update, form).then((res) => {
       // console.log(res.data);
@@ -170,6 +181,7 @@ function Users() {
         />
 
         <DialogForm
+          clean={setFields}
           title={"Registrar Usuarios"}
           open={openRegister}
           setOpen={setOpenRegister}
@@ -354,17 +366,13 @@ function Users() {
 
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={12} md={6}>
-                  <RolesSelect
-                    value={idroles_a}
-                    setValue={setIdroles_a}
-                    required
-                  />
+                  <RolesSelect value={idroles} setValue={setIdroles} required />
                 </Grid>
 
                 <Grid item xs={12} sm={12} md={6}>
                   <StatusSelect
-                    value={idstatus_a}
-                    setValue={setIdstatus_a}
+                    value={idstatus}
+                    setValue={setIdstatus}
                     required
                   />
                 </Grid>
@@ -379,8 +387,8 @@ function Users() {
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={12} md={6}>
                   <DocumentTypesSelect
-                    value={iddocument_types_a}
-                    setValue={setIddocument_types_a}
+                    value={iddocument_types}
+                    setValue={setIddocument_types}
                     readOnly
                     required
                   />
@@ -390,8 +398,8 @@ function Users() {
                   <TextFieldFilled
                     type={"number"}
                     label={"Identificación"}
-                    value={users_identification_a}
-                    setValue={setUsers_identification_a}
+                    value={users_identification}
+                    setValue={setUsers_identification}
                     required
                     readOnly
                   />
@@ -401,8 +409,8 @@ function Users() {
                   <TextFieldFilled
                     type={"text"}
                     label={"Nombres"}
-                    value={users_name_a}
-                    setValue={setUsers_name_a}
+                    value={users_name}
+                    setValue={setUsers_name}
                     required
                   />
                 </Grid>
@@ -411,8 +419,8 @@ function Users() {
                   <TextFieldFilled
                     type={"text"}
                     label={"Apellidos"}
-                    value={users_lastname_a}
-                    setValue={setUsers_lastname_a}
+                    value={users_lastname}
+                    setValue={setUsers_lastname}
                     required
                   />
                 </Grid>
@@ -421,8 +429,8 @@ function Users() {
                   <TextFieldFilled
                     type={"number"}
                     label={"Télefono"}
-                    value={users_phone_a}
-                    setValue={setUsers_phone_a}
+                    value={users_phone}
+                    setValue={setUsers_phone}
                     required
                   />
                 </Grid>
@@ -431,8 +439,8 @@ function Users() {
                   <TextFieldFilled
                     type={"text"}
                     label={"Correo"}
-                    value={users_email_a}
-                    setValue={setUsers_email_a}
+                    value={users_email}
+                    setValue={setUsers_email}
                     required
                     readOnly
                   />
@@ -448,11 +456,11 @@ function Users() {
               <Grid container spacing={3}>
                 <Grid item xs={12} sm={12} md={12}>
                   <LocationsSelect
-                    department={iddepartments_a}
-                    setDepartment={setIddepartments_a}
+                    department={iddepartments}
+                    setDepartment={setIddepartments}
                     requiredDepartment={true}
-                    city={idcities_a}
-                    setCity={setIdcities_a}
+                    city={idcities}
+                    setCity={setIdcities}
                     requiredCity={true}
                     disabledCity={false}
                   />
@@ -462,8 +470,8 @@ function Users() {
                   <TextFieldFilled
                     type={"text"}
                     label={"Dirección"}
-                    value={users_address_a}
-                    setValue={setUsers_address_a}
+                    value={users_address}
+                    setValue={setUsers_address}
                     required
                   />
                 </Grid>
@@ -480,8 +488,8 @@ function Users() {
                   <TextFieldFilled
                     type={"text"}
                     label={"Nombre"}
-                    value={users_contact_name_a}
-                    setValue={setUsers_contact_name_a}
+                    value={users_contact_name}
+                    setValue={setUsers_contact_name}
                   />
                 </Grid>
 
@@ -489,8 +497,8 @@ function Users() {
                   <TextFieldFilled
                     type={"number"}
                     label={"Télefono"}
-                    value={users_contact_phone_a}
-                    setValue={setUsers_contact_phone_a}
+                    value={users_contact_phone}
+                    setValue={setUsers_contact_phone}
                   />
                 </Grid>
               </Grid>
