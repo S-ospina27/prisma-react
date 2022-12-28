976b5e02893a1600c20c08e5545031555c6989ca
Sergio León <sergioleon4004@hotmail.com>
2022-12-28 11:05:59 -0500

    style: styles are modified in service orders

--- src/pages/ServiceOrders.jsx
+++ src/pages/ServiceOrders.jsx
@@ -4,9 +4,15 @@ import MenuItems from "../components/common/MenuItems";
 
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import AssignmentIcon from "@mui/icons-material/Assignment";
+import DialogForm from "../components/common/DialogForm";
 
 function ServiceOrders() {
   const [openCreateOrders, setOpenCreateOrders] = useState(false);
+
+  const handleCreateServiceOrders = (e) => {
+    e.preventDefault();
+    console.log("submitted");
+  };
 
   return (
     <Box mx={3} my={3}>
@@ -20,18 +26,30 @@ function ServiceOrders() {
         <MenuItems
           id={"operations"}
           iconButton={true}
-          label={<MoreVertIcon color={"primary"} />}
+          label={<MoreVertIcon color={"dark-blue"} />}
           items={[
             {
               type: "modal",
               name: "Crear Orden de Servicio",
-              icon: <AssignmentIcon color={"primary"} />,
+              icon: <AssignmentIcon color={"dark-blue"} />,
               setOpen: setOpenCreateOrders,
               idroles: [1],
             },
           ]}
         />
       </Box>
+
+      <DialogForm
+        open={openCreateOrders}
+        setOpen={setOpenCreateOrders}
+        title={"Registrar Ordenes de Servicio"}
+        button={{
+          type: "submit",
+          label: "Registrar",
+          onSubmit: handleCreateServiceOrders,
+        }}
+        content={<h1>Hola mundo</h1>}
+      />
     </Box>
   );
 }
