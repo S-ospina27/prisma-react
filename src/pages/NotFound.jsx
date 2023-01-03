e9e9148201947e67e2a2c401ec1c439907832752
Sergio León <sergioleon4004@hotmail.com>
2023-01-03 12:40:17 -0500

    ci: added files

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+function NotFound() {
+  return (
+    <Box m={5}>
+      <Typography variant="h3" gutterBottom>{"Page Not Found"}</Typography>
+    </Box>
+  );
+}
+
+export default NotFound;
