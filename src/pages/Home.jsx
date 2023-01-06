fe9748f5740f8619d1b1c217b5d77560b6457f53
Santiago Ospina <santiagoospinarojas27@gmail.com>
2023-01-06 14:45:53 -0500

    feat:added graph structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
+import Graphics from "../components/common/Graphics";
 
 function Home() {
   return (
+    <>
     <Box m={5}>
       <Typography variant="h3" gutterBottom>{"Home"}</Typography>
+      
     </Box>
+    <Grid item xs={12} sm={6} md={6}>
+    <Box >
+      <Graphics/>
+      
+    </Box>
+   </Grid> 
+    </>
   );
 }
 
