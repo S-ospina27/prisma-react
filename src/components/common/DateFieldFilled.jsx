328bc014f24e5cd926069caea94a6e2d52d3b117
Sergio León <sergioleon4004@hotmail.com>
2023-01-23 13:23:13 -0500

    fix: validation of rules by dates has been corrected

--- src/components/common/DateFieldFilled.jsx
+++ src/components/common/DateFieldFilled.jsx
@@ -16,7 +16,7 @@ function DateFieldFilled({
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"es-mx"}>
       <MobileDatePicker
         disableFuture
-        minDate={"2022-12-30"}
+        minDate={"2023-01-01"}
         views={["year", "month", "day"]}
         inputFormat={"YYYY-MM-DD"}
         label={label}
